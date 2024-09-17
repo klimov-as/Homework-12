4498b00cac2de20c0763b8340dace411132315a4
klimov-as <Raindal1995@gmail.com>
2024-09-17 22:16:26 +0700

    Add files via upload

--- Homework #12.py
+++ Homework #12.py
@@ -1,11 +1,13 @@
 def get_matrix(n, m, value):
     matrix = []
+    if m <= 0:
+        return []
     for i in range(n):
         matrix.append([])
         for j in range(m):
             matrix[i].append(value)
-
     return matrix
+
 
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
